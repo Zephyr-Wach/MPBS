36c2379a3baf7656aee7776216b06393da1feb68
zephyr <wangc2801@outlook.com>
2025-07-03 23:28:49 +0800

    add mpbs-gather

---
+++ mpbs-gather/src/main/java/com/zephyr/mpbsgather/MpbsGatherApplication.java
@@ -0,0 +1,13 @@
+package com.zephyr.mpbsgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpbsGatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpbsGatherApplication.class, args);
+    }
+
+}
---
+++ mpbs-gather/src/test/java/com/zephyr/mpbsgather/MpbsGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zephyr.mpbsgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpbsGatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
